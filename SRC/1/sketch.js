be9bd67d92a3c6f32c7ff3ad24a0b076e39075b5
timscn123 <timhlaug90@gmail.com>
2023-01-26 08:52:39 +0100

    Added linear function to SRC

--- SRC/1/sketch.js
+++ SRC/1/sketch.js
@@ -1,14 +1,21 @@
 let xShow, yShow;
 let x1, x2;
 let iterate;
-let a = 1,
-  b = 1,
-  c = 1,
-  y;
-let a2, b2, c2;
-let f, f2;
-let inputa, inputb, inputc, button;
-let graph;
+let a = 0.01,
+  b = 0,
+  c = 0;
+  
+let a2,
+  b2,
+  c2;
+
+let lina = 1,
+  linb = 1,
+  lina1 = 0,
+  linb1 = 0;
+
+let inputa, inputb, inputc, button, inputa1, inputb1, button1;
+let graph, linGraph;
 let v1, v2;
 
 function setup() {
@@ -22,15 +29,27 @@ function setup() {
   inputa = createInput();
   inputb = createInput();
   inputc = createInput();
+  inputa1 = createInput();
+  inputb1 = createInput();
+
   inputa.position(40, windowHeight - 90);
   inputb.position(40, windowHeight - 60);
   inputc.position(40, windowHeight - 30);
-  button = createButton("Go");
-  button.size(50, 20);
-  button.position(220, windowHeight - 30);
-  button.mousePressed(parseText);
+  inputa1.position(windowWidth - 250, windowHeight - 60);
+  inputb1.position(windowWidth - 250, windowHeight - 30);
 
-  graph = new Graph();
+  button = createButton("Go");
+  button1 = createButton("Go");
+
+  button.size(50, 20);
+  button1.size(50, 20);
+  button.position(220, windowHeight - 30);
+  button1.position(windowWidth - 70, windowHeight - 30);
+  button.mousePressed(parseText);
+  button1.mousePressed(parseTextLinear);
+
+  graph = new Graph("quadratic");
+  linGraph = new Graph("linear");
 
   rectMode(CENTER);
 }
@@ -46,52 +65,53 @@ function draw() {
   text("b:", 10, windowHeight - 60 + 15);
   text("c:", 10, windowHeight - 30 + 15);
   text(
-    "Current function: " + a + "x^2 + " + b + "x + " + c,
-    10,
+    "Quadratic function: " + a + "x^2 + " + b + "x + " + c,
+    35,
     windowHeight - 120 + 15
   );
+  text("a:", windowWidth - 285, windowHeight - 45);
+  text("b:", windowWidth - 285, windowHeight - 15);
+  text(
+    "Linear function: " + lina + "x + " + linb,
+    windowWidth - 250,
+    windowHeight - 75
+  );
+
   line(0, windowHeight / 2, windowWidth, windowHeight / 2);
   line(windowWidth / 2, 0, windowWidth / 2, windowHeight);
 
   textSize(20);
   text("(" + xShow + ", " + yShow + ")", 10, 25);
 
-  //rect(mouseX, mouseY, 50, 50)
   graph.draw();
+  linGraph.draw();
 }
-
-/*
-function Rapson(guess, f, f2) {
-  iterate = true;
-  x1 = guess;
-  while (iterate) {
-    if (!(x2 == x1)) {
-      x2 = x1 - f(x1) / f2(x1);
-    } else {
-      iterate = false;
-    }
-  }
-}
-*/
 
 function parseText() {
   a = parseFloat(inputa.value());
-
   b = parseFloat(inputb.value());
-
   c = parseFloat(inputc.value());
   graph.setValues(a, b, c);
 }
-
+function parseTextLinear() {
+  lina = parseFloat(inputa1.value());
+  linb = parseFloat(inputb1.value());
+  linGraph.setValues(lina, linb);
+}
 class Graph {
-  constructor() {
-    this.a = 1;
-    this.b = 1;
-    this.c = 1;
+  constructor(type) {
+    this.a = 0;
+    this.b = 0;
+    this.c = 0;
+    this.type = type;
   }
 
   calcY(x) {
     return -(a * x * x + b * x + c);
+  }
+
+  calcYLinear(x) {
+    return -(lina * x + linb);
   }
 
   setValues(a, b, c) {
@@ -104,10 +124,22 @@ class Graph {
     beginShape();
     noFill();
     strokeWeight(4);
-    for (let i = -windowWidth / 2 + 1; i < windowWidth; i++) {
-      v1.set(i + windowWidth / 2, graph.calcY(i) + windowHeight / 2);
-      vertex(v1.x, v1.y);
+
+    if (this.type == "quadratic") {
+      for (let i = -windowWidth / 2 + 1; i < windowWidth; i++) {
+        v1.set(i + windowWidth / 2, graph.calcY(i) + windowHeight / 2);
+        vertex(v1.x, v1.y);
+      }
+    } else if (this.type == "linear") {
+      for (let i = -windowWidth / 2 + 1; i < windowWidth; i++) {
+        v2.set(i + windowWidth / 2, linGraph.calcYLinear(i) + windowHeight / 2);
+        vertex(v2.x, v2.y);
+      }
     }
     endShape();
   }
+}
+
+function newtonRaphson() {
+
 }
