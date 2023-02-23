64d15b542b6dc52aa15779576c7ef70fc0cae0a4
timscn123 <timhlaug90@gmail.com>
2023-02-23 23:21:08 +0100

    Updated comments and added a guess input for SRC

--- SRC/1/sketch.js
+++ SRC/1/sketch.js
@@ -8,7 +8,7 @@ let ca, cb, cc, r;
 let a2, b2;
 let lina = 2,
   linb = 0;
-let inputa, inputb, inputc, button, inputa1, inputb1, button1;
+let inputa, inputb, inputc, button, inputa1, inputguess, inputb1, button1, buttonguess;
 let graph, linGraph;
 let v1, v2, collide;
 let guess = 1;
@@ -35,29 +35,34 @@ function setup() {
   inputa1.value(lina);
   inputb1 = createInput();
   inputb1.value(linb);
+  inputguess = createInput();
+  inputguess.value(guess);
 
   inputa.position(40, windowHeight - 90);
   inputb.position(40, windowHeight - 60);
   inputc.position(40, windowHeight - 30);
   inputa1.position(windowWidth - 250, windowHeight - 60);
   inputb1.position(windowWidth - 250, windowHeight - 30);
+  inputguess.position(10, 120);
 
   //creates buttons for the input fields
   button = createButton("Go");
   button1 = createButton("Go");
+  buttonguess = createButton("Go");
 
   button.size(50, 20);
   button1.size(50, 20);
+  buttonguess.size(50, 20);
   button.position(220, windowHeight - 30);
   button1.position(windowWidth - 70, windowHeight - 30);
+  buttonguess.position(190, 120);
   button.mousePressed(parseText);
   button1.mousePressed(parseTextLinear);
+  buttonguess.mousePressed(parseGuess);
 
   //creates graph and linGraph objects
   graph = new Graph();
   linGraph = new LinGraph();
-
-  
 }
 
 //runs every frame
@@ -74,19 +79,20 @@ function draw() {
   background(150);
 
   //creates textboxes for describing the input fields by "fixing" it to the sides of the window
+  text("Guess:", 10, 110);
   text("a:", 10, windowHeight - 90 + 15);
   text("b:", 10, windowHeight - 60 + 15);
   text("c:", 10, windowHeight - 30 + 15);
   text("a:", windowWidth - 285, windowHeight - 45);
   text("b:", windowWidth - 285, windowHeight - 15);
-  
+
   //displays the current quadratic function in the "Quadratic function:" textbox
   text(
     "Quadratic function: " + a + "x^2 + " + b + "x + " + c,
     35,
     windowHeight - 120 + 15
   );
-  
+
   //displays the current linear function in the "Linear function:" textbox
   text(
     "Linear function: " + lina + "x + " + linb,
@@ -96,7 +102,7 @@ function draw() {
 
   //runs newtonRaphson function to fill the "Collides at:" textbox
   newtonRaphson(a, b, c, lina, linb, guess, 0.0001, 1000);
-  
+
   //displays the collisionvector in the "Collides at:" textbox
   text(
     "Collides at: (" + collide.x.toFixed(0) + "," + collide.y.toFixed(0) + ")",
@@ -110,7 +116,7 @@ function draw() {
 
   //sets textsize to 20 and displays the mouse position in the coordinatesystem
   textSize(20);
-  text("(" + xShow + ", " + yShow + ")", 10, 25);
+  text("Pointer at: (" + xShow + ", " + yShow + ")", 10, 25);
 
   //draws the graphs using the draw() function in the Graph and LinGraph class
   graph.draw();
@@ -130,6 +136,10 @@ function parseTextLinear() {
   //parses the input fields for the linear function, converts them to float and sets the values of the linGraph object
   lina = parseFloat(inputa1.value());
   linb = parseFloat(inputb1.value());
+}
+
+function parseGuess() {
+  guess = parseFloat(inputguess.value());
 }
 
 //creates class Graph to draw the quadratic function
@@ -154,7 +164,7 @@ class Graph {
         -calcY(i, graph.type, false) + windowHeight / 2
       );
 
-      //uses P5.js vertex function to draw the graph
+      //uses P5.js vertex function to draw the graph from vector v1
       vertex(v1.x, v1.y);
     }
     //ends the shape
@@ -184,7 +194,7 @@ class LinGraph {
         -calcY(i, linGraph.type, false) + windowHeight / 2
       );
 
-      //uses P5.js vertex function to draw the graph
+      //uses P5.js vertex function to draw the graph from vector v2
       vertex(v2.x, v2.y);
     }
     //ends the shape
@@ -195,21 +205,25 @@ class LinGraph {
 //calculates the y value of the mathematical function depending on the type of function and whether it is the function or the derivative
 function calcY(x, type, diff) {
   //quadratic function
-  if (type == "quadratic" && diff == false) {
-    return a * x * x + b * x + c;
+  if (type == "quadratic") {
+    if (diff == false) {
+      return a * x * x + b * x + c;
 
-    //derivative of quadratic function
-  } else if (type == "quadratic" && diff == true) {
-    return 2 * a * x + b;
+      //derivative of quadratic function
+    } else if (diff == true) {
+      return 2 * a * x + b;
+    }
   }
 
   //linear function
-  if (type == "linear" && diff == false) {
-    return lina * x + linb;
+  if (type == "linear") {
+    if (diff == false) {
+      return lina * x + linb;
 
-    //derivative of linear function
-  } else if (type == "linear" && diff == true) {
-    return linb;
+      //derivative of linear function
+    } else if (diff == true) {
+      return lina;
+    }
   }
 }
 
